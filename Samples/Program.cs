daeb375c162f7ff4df39d84a1fb1ec681ab8e093
Frank A. Krueger <fak@praeclarum.org>
2017-06-12 15:35:19 -0700

    Add Samples project

---
+++ Samples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Ooui;
+
+namespace Samples
+{
+    class Program
+    {
+        static int Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+            var server = new Server ();
+            server.RunAsync ("http://*:8080/").Wait ();
+            return 0;
+        }
+    }
+}
