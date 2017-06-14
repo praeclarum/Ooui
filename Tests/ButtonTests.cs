ab5a1b78295930b3c42ffc455f3fa53d5fb2dfb9
Frank A. Krueger <fak@praeclarum.org>
2017-06-13 20:42:06 -0700

    Add unit tests

---
+++ Tests/ButtonTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Ooui;
+
+namespace Tests
+{
+    [TestClass]
+    public class ButtonTests
+    {
+        [TestMethod]
+        public void DefaultCtor ()
+        {
+            var b = new Button ();
+            Assert.AreEqual ("", b.TextContent);
+        }
+    }
+}
