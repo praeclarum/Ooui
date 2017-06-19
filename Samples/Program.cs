c6979c1d636f8c2f2d30a2a01bba940c611e767e
Frank A. Krueger <fak@praeclarum.org>
2017-06-18 18:42:28 -0700

    Add --port option to samples
    Fixes #6

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -7,6 +7,22 @@ namespace Samples
     {
         static void Main (string[] args)
         {
+            for (var i = 0; i < args.Length; i++) {
+                var a = args[i];
+                switch (args[i]) {
+                    case "-p" when i + 1 < args.Length:
+                    case "--port" when i + 1 < args.Length:
+                        {
+                            int p;
+                            if (int.TryParse (args[i + 1], out p)) {
+                                UI.Port = p;
+                            }
+                            i++;
+                        }
+                        break;
+                }
+            }
+
             new ButtonSample ().Publish ();
             new TodoSample ().Publish ();
 
