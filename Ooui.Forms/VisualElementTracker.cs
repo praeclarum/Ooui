71611af1e2f04d8ded906ad5447ab0f18b72fefc
Frank A. Krueger <fak@praeclarum.org>
2017-11-09 12:34:08 -0800

    Update styling

---
+++ Ooui.Forms/VisualElementTracker.cs
@@ -0,0 +1,12 @@
+﻿namespace Ooui.Forms
+{
+	internal class VisualElementTracker
+	{
+		private VisualElementRenderer<object> visualElementRenderer;
+
+		public VisualElementTracker(VisualElementRenderer<object> visualElementRenderer)
+		{
+			this.visualElementRenderer = visualElementRenderer;
+		}
+	}
+}
