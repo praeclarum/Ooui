d351b07be46191b461d4a8d4069f6ceaeb959212
Frank A. Krueger <fak@praeclarum.org>
2017-11-10 11:32:51 -0800

    Add BoxRenderer and fix color messaging

--- Ooui/Color.cs
+++ Ooui/Color.cs
@@ -1,20 +1,21 @@
 using System;
-
+using Newtonsoft.Json;
 using StyleValue = System.Object;
 
 namespace Ooui
 {
-    public struct Color
+    [Newtonsoft.Json.JsonConverter (typeof (ColorJsonConverter))]
+    public struct Color : IEquatable<Color>
     {
         public byte R, G, B, A;
 
         public Color (byte r, byte g, byte b, byte a)
-		{
-			R = r;
-			G = g;
-			B = b;
-			A = a;
-		}
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
 
         public double Red {
             get => R / 255.0;
@@ -33,9 +34,70 @@ namespace Ooui
             set => A = value >= 1.0 ? (byte)255 : ((value <= 0.0) ? (byte)0 : (byte)(value * 255.0 + 0.5));
         }
 
-		public static Color FromStyleValue (StyleValue styleColor)
-		{
+        public override bool Equals (object obj)
+        {
+            if (obj is Color other)
+                return R == other.R && G == other.G && B == other.B && A == other.A;
+            return false;
+        }
+
+        public bool Equals (Color other) => R == other.R && G == other.G && B == other.B && A == other.A;
+
+        public override int GetHashCode () => R.GetHashCode () + G.GetHashCode () * 2 + B.GetHashCode () * 3 + A.GetHashCode () * 5;
+
+        public static Color FromStyleValue (StyleValue styleColor)
+        {
+            if (styleColor is Color c)
+                return c;
+            if (styleColor is string s)
+                return Parse (s);
             return Colors.Clear;
-		}
-	}
+        }
+
+        public static Color Parse (string styleValue)
+        {
+            if (string.IsNullOrWhiteSpace (styleValue) || styleValue.Length < 4)
+                throw new ArgumentException ("Cannot parse empty strings", nameof (styleValue));
+            
+            if (styleValue.Length > 32)
+                throw new ArgumentException ("Color string is too long", nameof (styleValue));
+
+            if (styleValue == "inherit")
+                return Colors.Clear;
+            
+            //if (styleValue[0] == '#' && styleValue.Length == 4) {
+            //}
+
+            //if (styleValue[0] == '#' && styleValue.Length == 7) {
+            //}
+
+            throw new ArgumentException ($"Cannot parse color string `{styleValue}`", nameof (styleValue));
+        }
+
+        public override string ToString ()
+        {
+            if (A == 255)
+                return string.Format ("#{0:x2}{1:x2}{2:x2}", R, G, B);
+            return string.Format ("rgba({0},{1},{2},{3})", R, G, B, A / 255.0);
+        }
+    }
+
+    class ColorJsonConverter : Newtonsoft.Json.JsonConverter
+    {
+        public override bool CanConvert (Type objectType)
+        {
+            return objectType == typeof (Color);
+        }
+
+        public override object ReadJson (JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var str = reader.ReadAsString ();
+            return Color.Parse (str);
+        }
+
+        public override void WriteJson (JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue (value.ToString ());
+        }
+    }
 }
