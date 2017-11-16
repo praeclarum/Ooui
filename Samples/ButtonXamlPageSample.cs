f550c55e909addad9ef920813ec95a570e36a8cb
Frank A. Krueger <fak@praeclarum.org>
2017-11-15 23:59:40 -0600

    Add XAML example

---
+++ Samples/ButtonXamlPageSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ooui;
+using Xamarin.Forms;
+
+namespace Samples
+{
+    public class ButtonXamlPageSample : ISample
+    {
+        public string Title => "Xamarin.Forms Button XAML";
+
+        public Ooui.Element CreateElement ()
+        {
+            var page = new ButtonXaml.ButtonXamlPage ();
+            return page.GetOouiElement ();
+        }
+    }
+}
