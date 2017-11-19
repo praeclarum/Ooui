010d1f75d00567025ba0d9ee3e68c63a6d97f0f5
Frank A. Krueger <fak@praeclarum.org>
2017-11-19 12:49:17 -0600

    Organize samples

---
+++ Samples/ButtonXamlSample.cs
@@ -0,0 +1,16 @@
+﻿using Ooui;
+using Xamarin.Forms;
+
+namespace Samples
+{
+    public class ButtonXamlSample : ISample
+    {
+        public string Title => "Xamarin.Forms Button XAML";
+
+        public Ooui.Element CreateElement ()
+        {
+            var page = new ButtonXaml.ButtonXamlPage ();
+            return page.GetOouiElement ();
+        }
+    }
+}
--- Samples/DisplayAlertPage.xaml.cs
+++ Samples/DisplayAlertPage.xaml.cs
@@ -7,9 +7,9 @@ using Xamarin.Forms.Xaml;
 namespace Samples
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class DisplayAlertSample : ContentPage
+	public partial class DisplayAlertPage : ContentPage
 	{
-		public DisplayAlertSample ()
+		public DisplayAlertPage ()
 		{
 			InitializeComponent ();
 		}
@@ -19,11 +19,5 @@ namespace Samples
             var result = await DisplayAlert("Alert Message", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.", "YES", "NO");
             await DisplayAlert("Alert Response", $"You selected value: {result}", "OK");
         }
-
-        public void Publish()
-        {
-            var sample = new XamlPageSample();
-            UI.Publish("/display-alert", sample.CreateElement(this));
-        }
-	}
+    }
 }
