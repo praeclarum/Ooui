9d50826c1886c1abbebb953044ff85f369998b17
Frank A. Krueger <fak@praeclarum.org>
2017-12-10 20:17:35 -0800

    Improve the XAML Editor

--- Samples/XamlPreviewPage.xaml.cs
+++ Samples/XamlPreviewPage.xaml.cs
@@ -23,10 +23,14 @@ namespace Samples
     <ColumnDefinition Width=""*"" />
     <ColumnDefinition Width=""*"" />
   </Grid.ColumnDefinitions>
-  <Label Text=""Top Left"" Grid.Row=""0"" Grid.Column=""0"" />
-  <Label Text=""Top Right"" Grid.Row=""0"" Grid.Column=""1"" />
-  <Label Text=""Bottom Left"" Grid.Row=""1"" Grid.Column=""0"" />
-  <Label Text=""Bottom Right"" Grid.Row=""1"" Grid.Column=""1"" />
+  <StackLayout Grid.Row=""0"" Grid.Column=""0"">
+    <Label Text=""Top Left"" />
+    <Entry Placeholder=""I'm ready for some text"" />
+    <Button Text=""I'm a button, but I don't do anything"" />
+  </StackLayout>
+  <Label Text=""Top Right"" Grid.Row=""0"" Grid.Column=""1"" TextColor=""White"" BackgroundColor=""#c5000b"" />
+  <Label Text=""Bottom Left"" Grid.Row=""1"" Grid.Column=""0"" TextColor=""Black"" BackgroundColor=""#ffd320"" />
+  <Label Text=""Bottom Right"" Grid.Row=""1"" Grid.Column=""1"" TextColor=""White"" BackgroundColor=""#008000"" />
 </Grid>
 </ContentView>";
             editor.TextChanged += (sender, e) => DisplayXaml ();
