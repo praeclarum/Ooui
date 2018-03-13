794e27dd799000eceb1c778b715c38dfe7454377
Frank A. Krueger <fak@praeclarum.org>
2018-03-13 16:49:21 -0700

    Add charset to wasm HTML

--- Ooui.Wasm.Build.Tasks/BuildDistTask.cs
+++ Ooui.Wasm.Build.Tasks/BuildDistTask.cs
@@ -260,6 +260,7 @@ namespace Ooui.Wasm.Build.Tasks
                 w.Write (@"<!DOCTYPE html>
 <html>
 <head>
+    <meta charset=""utf-8"" />
     <meta name=""viewport"" content=""width=device-width, initial-scale=1"" />
     <link rel=""stylesheet"" href=""https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap.min.css"" />
     <link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"">
