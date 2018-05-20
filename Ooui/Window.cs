6c01c869f52e027550356ef6cce4f9ee8d9e7b4d
Lee McPherson <leemcpherson@hotmail.com>
2018-05-19 20:57:00 -0700

    Undoing unnecessary changes to Window and removed Location file from Ooui main

--- Ooui/Window.cs
+++ Ooui/Window.cs
@@ -2,6 +2,21 @@
 {
     public class Window : EventTarget
     {
+        string location = "";
+
+        public string Location
+        {
+            get => location;
+            set
+            {
+                if (string.IsNullOrEmpty(value) || location == value)
+                    return;
+                location = value;
+                Send(Message.Set("window", "location", value));
+                OnPropertyChanged("Location");
+            }
+        }
+
         public Window ()
             : base ("window")
         {
