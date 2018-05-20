511170c452196f1ea6b0d9384a8772397c9ed1c6
Lee McPherson <leemcpherson@hotmail.com>
2018-05-19 20:46:47 -0700

    Undo window.cs
    Didn't need this and forgot to remove it.

--- Ooui/Window.cs
+++ Ooui/Window.cs
@@ -2,18 +2,11 @@
 {
     public class Window : EventTarget
     {
-        public Location Location { get; } = new Location();
-
         public Window ()
             : base ("window")
         {
             Id = "window";
-            Location.MessageSent += Proxy_MessageSent;
-        }
-
-        void Proxy_MessageSent(Message message)
-        {
-            Send(message);
+           
         }
     }
 }
