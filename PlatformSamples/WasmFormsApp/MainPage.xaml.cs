95819fd23d3cd32adb586f5952f46b9c74323387
Kevin Gliewe <kevingliewe@outlook.de>
2018-07-24 22:24:54 +0200

    Update Xamarin.Forms to v3.1.0.637273 and add Wasm example

---
+++ PlatformSamples/WasmFormsApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WasmFormsApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
